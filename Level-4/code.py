b92897c64c8cd0e317d2e28e89f299f88def87f7
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-04-09 10:21:23 +0000

    feat(level4): update `update_stock_price`

--- Level-4/code.py
+++ Level-4/code.py
@@ -161,10 +161,10 @@ class DB_CRUD_ops(object):
 
             res = "[METHOD EXECUTED] update_stock_price\n"
             # UPDATE stocks SET price = 310.0 WHERE symbol = 'MSFT'
-            query = "UPDATE stocks SET price = '%d' WHERE symbol = '%s'" % (price, stock_symbol)
-            res += "[QUERY] " + query + "\n"
+            query = "UPDATE stocks SET price = ? WHERE symbol = ?"
+            res += "[QUERY] " + (query.replace("?", "'%d'", 1) % price).replace("?", f"'{stock_symbol}'") + "\n"
 
-            cur.execute(query)
+            cur.execute(query, (price, stock_symbol,))
             db_con.commit()
             query_outcome = cur.fetchall()
             for result in query_outcome:
